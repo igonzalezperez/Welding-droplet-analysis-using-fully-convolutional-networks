b0b38cb427b35c3fdffc269eef76c7491a19d608
Ivan Gonzalez <ivan.gonzalez@ug.uchile.cl>
2021-03-03 19:19:15 -0300

    Add frame display limit to animation.

--- Code/plot_results.py
+++ Code/plot_results.py
@@ -21,13 +21,13 @@ from utils.misc import set_size
 matplotlib.rcParams['animation.ffmpeg_path'] = os.path.abspath(
     'C:\\ffmpeg\\bin\\ffmpeg.exe')
 # output images for LaTex
-matplotlib.use("pgf")
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-})
+# matplotlib.use("pgf")
+# matplotlib.rcParams.update({
+#     "pgf.texsystem": "pdflatex",
+#     'font.family': 'serif',
+#     'text.usetex': True,
+#     'pgf.rcfonts': False,
+# })
 # set style
 sns.set()
 
@@ -39,9 +39,9 @@ sns.set()
 # LaTex \textwidth = 472.03123 pt. Useful for figure sizing
 
 ARCHITECTURE_NAME = 'unet'
-DATASET = 'Spray'
-N_FILTERS = 8
-BATCH_SIZE_TRAIN = 8
+DATASET = 'Globular'
+N_FILTERS = 32
+BATCH_SIZE_TRAIN = 16
 EPOCHS = 200
 
 DATA_DIR_RGB = os.path.join(
@@ -53,8 +53,8 @@ PREDS_DIR = os.path.join('Output', 'Predictions',
 GEOMETRY_DIR = os.path.join('Output', 'Geometry',
                             f'{ARCHITECTURE_NAME.lower()}_{DATASET.lower()}_{N_FILTERS}_{BATCH_SIZE_TRAIN}_{EPOCHS}_geometry.pickle')
 
-P1 = 334*10 ** (-6)  # period between frames in seconds
-P2 = 333*10 ** (-6)
+FPS = 3000.0
+PERIOD = 1/FPS
 PX_TO_MM = 4.396153846153846 * 10**(-2)
 
 DATA_RGB = np.load(DATA_DIR_RGB)
@@ -139,9 +139,14 @@ def plot_areas():
     fig = plt.figure()
     grid_spec = fig.add_gridspec(2, 1)
     ax0 = fig.add_subplot(grid_spec[0, 0])
+    ax0.xaxis.tick_top()
+    ax0.set_xlabel('Frame')
+    ax0.xaxis.set_label_position('top')
+    ax0.set_ylabel(r'Area [$mm^2$]')
     with sns.axes_style('dark'):
         ax1 = fig.add_subplot(grid_spec[1, 0])
-
+    ax1.set_xticks([])
+    ax1.set_yticks([])
     if len(areas) == 0:
         ax0.plot(0, 0, 'bo')
     else:
@@ -162,8 +167,8 @@ def plot_areas():
         else:
             original_area.append(0)
     smooth_area = smooth_signal(np.array(original_area), 51)
-    peaks = find_peaks(-smooth_area, width=50,
-                       prominence=300, height=-2500)
+    peaks = find_peaks(-smooth_area, height=-5, width=50, prominence=5)
+
     for i, data_i in enumerate(data):
         areas, img = data_i
         if len(areas) == 0:
@@ -178,11 +183,18 @@ def plot_areas():
             ax0.axvline(x=i, color='k', alpha=0.4, linestyle='--')
         with sns.axes_style('dark'):
             image_ax.set_data(img)
-        fig.suptitle(f'Frame #{i}')
-        plt.savefig(os.path.join('Output', 'Videos', 'area_spray', f'{i}.jpg'))
+        if (i+1) % 1000 == 0:
+            ax0.clear()
+            ax0.xaxis.tick_top()
+            ax0.set_xlabel('Frame')
+            ax0.xaxis.set_label_position('top')
+            ax0.set_ylabel(r'Area [$mm^2$]')
+        plt.pause(.001)
+    plt.show()
+    # plt.savefig(os.path.join('Output', 'Videos', 'area_spray', f'{i}.jpg'))
 
 
-def animate_areas(save=False):
+def animate_areas(save=False, max_width=200):
     '''
     DOC
     '''
@@ -194,9 +206,15 @@ def animate_areas(save=False):
     fig = plt.figure()
     grid_spec = fig.add_gridspec(2, 1)
     ax0 = fig.add_subplot(grid_spec[0, 0])
+    ax0.xaxis.tick_top()
+    ax0.set_xlabel('Frame')
+    ax0.xaxis.set_label_position('top')
+    ax0.set_ylabel(r'Area [$mm^2$]')
+
     with sns.axes_style('dark'):
         ax1 = fig.add_subplot(grid_spec[1, 0])
-
+    ax1.set_xticks([])
+    ax1.set_yticks([])
     if len(areas) == 0:
         ax0.plot(0, 0, 'bo')
     else:
@@ -217,8 +235,7 @@ def animate_areas(save=False):
         else:
             original_area.append(0)
     smooth_area = smooth_signal(np.array(original_area), 51)
-    peaks = find_peaks(-smooth_area, width=50,
-                       prominence=300, height=-2500)
+    peaks = find_peaks(-smooth_area, height=-5, width=50, prominence=5)
 
     def animate(i):
         areas, img = next(data)
@@ -235,6 +252,12 @@ def animate_areas(save=False):
         with sns.axes_style('dark'):
             image_ax.set_data(img)
         plt.title(f'Frame #{i}')
+        if (i+1) % max_width == 0:
+            ax0.clear()
+            ax0.xaxis.tick_top()
+            ax0.set_xlabel('Frame')
+            ax0.xaxis.set_label_position('top')
+            ax0.set_ylabel(r'Area [$mm^2$]')
         return image_ax,
 
     ani = FuncAnimation(fig, animate, save_count=N_FRAMES-1)
@@ -331,7 +354,7 @@ def plot_areas_with_masks():
                                  'area_mask_spray', f'{i}.jpg'))
 
 
-def animate_areas_with_masks(save=False):
+def animate_areas_with_masks(save=False, max_width=200):
     '''
     DOC
     '''
@@ -344,10 +367,18 @@ def animate_areas_with_masks(save=False):
     fig = plt.figure()
     grid_spec = fig.add_gridspec(2, 2)
     ax0 = fig.add_subplot(grid_spec[0, :])
+    ax0.xaxis.tick_top()
+    ax0.set_xlabel('Frame')
+    ax0.xaxis.set_label_position('top')
+    ax0.set_ylabel(r'Area [$mm^2$]')
+
     with sns.axes_style('dark'):
         ax1 = fig.add_subplot(grid_spec[1, 0])
         ax2 = fig.add_subplot(grid_spec[1, 1])
-
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax2.set_xticks([])
+    ax2.set_yticks([])
     if len(areas) == 0:
         ax0.plot(0, 0, 'bo')
     else:
@@ -410,7 +441,13 @@ def animate_areas_with_masks(save=False):
             single_droplet_2.set_data([], [])
             multi_droplet_1.set_data([], [])
             multi_droplet_2.set_data([], [])
-        fig.suptitle(f'Frame #{i}')
+        ax1.set_title(f'Frame #{i}')
+        if (i+1) % max_width == 0:
+            ax0.clear()
+            ax0.xaxis.tick_top()
+            ax0.set_xlabel('Frame')
+            ax0.xaxis.set_label_position('top')
+            ax0.set_ylabel(r'Area [$mm^2$]')
         return image_ax, mask_ax, single_droplet_1, multi_droplet_1, single_droplet_2, multi_droplet_2,
 
     ani = FuncAnimation(fig, animate, save_count=N_FRAMES-1)
@@ -615,127 +652,4 @@ def plot_smooth_area():
 
 # %% MAIN
 if __name__ == "__main__":
-    # animate_centroids(save=True)
-    # animate_centroids_with_masks(save=True)
-    # animate_areas(save=False)
-    # plot_areas_with_masks()
-    # animate_areas_with_masks(save=False)
-    # plot_smooth_area()
-    plot_strip(500, save=True)
-    # def plot_perimeters(cont=True):
-    # geom = GEOMETRY['perimeters']
-
-    # if cont:
-    #     _, (ax1, ax2) = plt.subplots(
-    #         2, 1)
-    #     for i in range(len(geom)):
-    #         ar = geom[i]
-    #         if len(ar) > 1:
-    #             for j in ar:
-    #                 ax1.plot(i, j*PX_TO_MM, 'rx')
-    #         elif len(ar) == 1:
-    #             ax1.plot(i, ar[0]*PX_TO_MM, 'k.')
-    #         elif len(ar) == 0:
-    #             ax1.plot(i, 0, 'b*')
-    #         ax2.imshow(cv2.imread('HSV Frames\\Test\\' +
-    #                               DATASET + '\\' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE))
-    #         ax1.set_ylabel('Perimeter (mm)')
-    #         plt.pause(.0001)
-    #     plt.show()
-    # else:
-    #     single_droplet, single_droplet_idx = ([], [])
-    #     multi_droplet, multi_droplet_idx = ([], [])
-    #     zero_droplet, zero_droplet_idx = ([], [])
-
-    #     for i in range(len(geom)):
-    #         ar = geom[i]
-    #         if len(ar) > 1:
-    #             for j in ar:
-    #                 multi_droplet.append(j*PX_TO_MM)
-    #                 multi_droplet_idx.append(i)
-    #         elif len(ar) == 1:
-    #             single_droplet.append(ar[0]*PX_TO_MM)
-    #             single_droplet_idx.append(i)
-    #         elif len(ar) == 0:
-    #             zero_droplet.append(0)
-    #             zero_droplet_idx.append(i)
-    #     plt.plot(single_droplet_idx, single_droplet, 'k-.')
-    #     plt.plot(multi_droplet_idx, multi_droplet, 'rx')
-    #     plt.plot(zero_droplet_idx, zero_droplet, 'b*')
-
-    #     plt.ylim([0, 500*PX_TO_MM])
-    #     plt.tight_layout()
-    #     plt.savefig('perimeters.pdf')
-    #     plt.show()
-
-    # # def plot_areas(cont=True):
-    #     geom = GEOMETRY['areas']
-    #     if cont:
-    #         _, (ax1, ax2) = plt.subplots(
-    #             2, 1)
-    #         for i in range(len(geom)):
-    #             ar = geom[i]
-    #             if len(ar) > 1:
-    #                 for j in ar:
-    #                     ax1.plot(i, j*PX_TO_MM**2, 'rx')
-    #             elif len(ar) == 1:
-    #                 ax1.plot(i, ar[0]*PX_TO_MM**2, 'k.')
-    #             elif len(ar) == 0:
-    #                 ax1.plot(i, 0, 'b*')
-    #             ax2.imshow(cv2.imread('HSV Frames\\Test\\' +
-    #                                   DATASET + '\\' + str(i) + '.jpg', cv2.IMREAD_GRAYSCALE))
-    #             ax1.set_ylabel(r'Area ($mm^2$)')
-    #             ax1.set_ylim([0, 500*PX_TO_MM])
-    #             plt.tight_layout()
-    #             plt.savefig(f'Graphs\\{i}.jpg', format='jpg')
-    #     else:
-
-    #         single_droplet, single_droplet_idx = ([], [])
-    #         multi_droplet, multi_droplet_idx = ([], [])
-    #         zero_droplet, zero_droplet_idx = ([], [])
-    #         dt = cp.time_period([P1, P2, P2, P2])
-    #         for i in range(len(geom)):
-    #             ar = geom[i]
-    #             if len(ar) == 0:
-    #                 single_droplet.append(0)
-    #             elif len(ar) > 1:
-    #                 single_droplet.append(sum(ar)*PX_TO_MM**2*0.01)
-    #             else:
-    #                 single_droplet.append(max(ar)*PX_TO_MM**2*0.01)
-    #             # if len(ar) > 1:
-    #             #     for j in ar:
-    #             #         multi_droplet.append(j*PX_TO_MM**2)
-    #             #         multi_droplet_idx.append(i)
-    #             # elif len(ar) == 1:
-    #             #     single_droplet.append(ar[0]*PX_TO_MM**2)
-    #             #     single_droplet_idx.append(i)
-    #             # elif len(ar) == 0:
-    #             #     zero_droplet.append(0)
-    #             #     zero_droplet_idx.append(i)
-    #         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(16, 8))
-    #         # smooth_droplet = smooth(np.array(single_droplet), 100)
-
-    #         # peaks = scipy.signal.find_peaks(-smooth_droplet, height=-.04)
-    #         # for i, peak in enumerate(peaks[0]):
-    #         #     if i == 0:
-    #         #         ax1.axvline(x=peak, color='b', alpha=0.4, label='detachment')
-    #         #     else:
-    #         #         ax1.axvline(x=peak, color='b', alpha=0.4)
-    #         ax1.plot(range(len(single_droplet)), single_droplet,
-    #                  'k-', alpha=.5, label='area')
-    #         # ax1.plot(range(len(multi_droplet_idx)), multi_droplet,
-    #         #          'rx', alpha=.5, label='area')
-    #         # ax1.plot(range(len(zero_droplet_idx)), zero_droplet,
-    #         #          'bx', alpha=.5, label='area')
-    #         # # ax1.plot(range(len(smooth_droplet)), smooth_droplet,
-    #         #          'k-', label='smoothed area')
-
-    #         # plt.plot(multi_droplet_idx, multi_droplet, 'rx')
-    #         # plt.plot(zero_droplet_idx, zero_droplet, 'b*')
-
-    #         ax1.set_ylim([0, 2200*PX_TO_MM**2*0.01])
-    #         ax1.set_ylabel(r'Area ($cm^2$)')
-    #         ax1.legend()
-    #         # plt.savefig('areas.pdf')
-
-    #         return ax2  # , peaks
+    animate_areas_with_masks(save=True, max_width=1000)
